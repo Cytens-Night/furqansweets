7142a7a459e7075f438e0ec8340c6186a53dd9c3
Cytens <businesstopman@gmail.com>
2026-09-02 13:17:15 +0100

    Fix CRM iframe taking up correct full height without scrolling

--- src/pages/Access.jsx
+++ src/pages/Access.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 function Access() {
   return (
-    <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
+    <div style={{ 
+      position: 'fixed', 
+      top: 0, 
+      left: 0, 
+      right: 0, 
+      bottom: 0, 
+      zIndex: 9999, 
+      backgroundColor: '#0F0D0C' 
+    }}>
       <iframe src="/access.html" title="CRM Access" style={{ width: '100%', height: '100%', border: 'none' }} />
     </div>
   );
